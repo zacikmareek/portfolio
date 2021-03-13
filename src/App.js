0e2ab4fb078efb1ed8b9b4e0c1e61a022049d84c
zacikmareek <zacik.mareek@gmail.com>
2021-03-13 18:42:02 +0100

    completed header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
-import Navbar from "./components/Navbar"
+import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "react-particles-js";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <Navbar/>
+    <>
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles:{
+            number:{
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "square",
+              stroke: {
+                width: 6,
+                color: "#ef4035"
+              }
+            }
+          }
+        }}
+      />
+      <Navbar/>
+      <Header/>
+    </>
   );
 }
 
