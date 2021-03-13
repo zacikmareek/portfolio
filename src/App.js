fecaaba9ca0a70d5d94878f87bb1ccc107aaf847
zacikmareek <zacik.mareek@gmail.com>
2021-03-13 22:24:14 +0100

    created services

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
+import Services from "./components/Services";
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
       <Navbar/>
       <Header/>
       <About/>
+      <Services/>
     </>
   );
 }
